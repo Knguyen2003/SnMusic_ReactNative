3d9c7acff09c81485cd36b7c32ca758d718fb1b8
ANHHAI <haianhchidan@gmail.com>
2024-11-08 12:02:43 +0700

    create screen PlaylistDetail_AudioListing

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -2,14 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LaunchScreen from '../screens/LaunchScreen';
 import NaviBottom from '../navigation/NavigationBottomTab';
+import PlaylistDetail_AudioListing from '../screens/PlaylistDetail_AudioListing';
 
 const Stack = createStackNavigator();
 
 const Navi = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LaunchScreen" >
+      <Stack.Navigator initialRouteName="PlaylistDetail_AudioListing" >
         <Stack.Screen name="LaunchScreen" component={LaunchScreen} />
+        <Stack.Screen name="PlaylistDetail_AudioListing" component={PlaylistDetail_AudioListing} />
         <Stack.Screen name="NaviBottom" component={NaviBottom} />
       </Stack.Navigator>
     </NavigationContainer>
