43a45245133fddf84986d1744b870324b86b5582
ANHHAI <haianhchidan@gmail.com>
2024-11-09 12:36:58 +0700

    YourPlaylist

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -7,19 +7,21 @@ import PlayAnAudio from '../screens/PlayAnAudio';
 import ArtistProfile from '../screens/ArtistProfile';
 import HomeAudioListing from '../screens/HomeAudioListing';
 import YourLibrary from '../screens/YourLibrary';
+import YourPlaylist from '../screens/YourPlaylist';
 
 const Stack = createStackNavigator();
 
 const Navi = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="YourLibrary" >
+      <Stack.Navigator initialRouteName="YourPlaylist" >
         <Stack.Screen name="LaunchScreen" component={LaunchScreen} />
         <Stack.Screen name="HomeAudioListing" component={HomeAudioListing} />
         <Stack.Screen name="PlaylistDetail_AudioListing" component={PlaylistDetail_AudioListing} />
         <Stack.Screen name="PlayAnAudio" component={PlayAnAudio} />
         <Stack.Screen name="ArtistProfile" component={ArtistProfile} />
         <Stack.Screen name="YourLibrary" component={YourLibrary} />
+        <Stack.Screen name="YourPlaylist" component={YourPlaylist} />
         <Stack.Screen name="NaviBottom" component={NaviBottom} />
       </Stack.Navigator>
     </NavigationContainer>
