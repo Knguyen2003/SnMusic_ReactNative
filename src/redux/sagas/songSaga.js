e5b332b743e643fe874df1f02a451fe80df175f2
Knguyen2003 <nguyenphan1122k3@gmail.com>
2024-11-11 18:04:12 +0700

    CallAPI firebase with ReduxSaga

---
+++ src/redux/sagas/songSaga.js
@@ -0,0 +1,29 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import { collection, getDocs } from "firebase/firestore";
+import {
+  fetchArtistsSuccess,
+  fetchArtistsFailure,
+  FETCH_ARTISTS_REQUEST,
+} from "../actions/artistAction";
+import { db } from "../../component/configFirebase";
+
+// Hàm thực thi lấy danh sách nhạc từ Firestore
+function* fetchArtists() {
+  try {
+    const artistsCollection = collection(db, "artists");
+    const snapshot = yield call(() => getDocs(artistsCollection)); // Lấy các tài liệu trong collection "artists"
+    const artistsList = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    yield put(fetchArtistsSuccess(artistsList)); // Dispatch action thành công
+  } catch (error) {
+    yield put(fetchArtistsFailure(error.message)); // Dispatch action thất bại
+  }
+}
+
+// Watcher Saga để theo dõi action FETCH_ARTISTS_REQUEST
+export function* watchFetchSongs() {
+  yield takeLatest(FETCH_ARTISTS_REQUEST, fetchArtists);
+}
