99d4399c07ca5354a871a45c49b44a716c937b54
Knguyen2003 <nguyenphan1122k3@gmail.com>
2024-11-21 23:28:28 +0700

    callAPI with ReduxSaga-supabase

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,7 +1,6 @@
 import { all } from "redux-saga/effects";
-import { watchFetchSongs } from "./songSaga";
 import { watchFetchArtists } from "./artistSaga";
 
 export default function* rootSaga() {
-  yield all([watchFetchSongs(), watchFetchArtists()]);
+  yield all([watchFetchArtists()]);
 }
